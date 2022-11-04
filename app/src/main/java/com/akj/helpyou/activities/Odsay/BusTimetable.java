a1cd5c4ecb76e2d76d93194a1ed4d669ab6fb1bb
hosun1997 <hwanghsun@suwon.ac.kr>
2022-11-04 11:20:52 +0900

    api start

---
+++ app/src/main/java/com/akj/helpyou/activities/Odsay/BusTimetable.java
@@ -0,0 +1,5 @@
+package com.akj.helpyou.activities.Odsay;
+// 버스 시간 받아오는 Json
+public class BusTimetable extends Thread {
+
+}
--- app/src/main/java/com/akj/helpyou/activities/ResultRouteActivity.java
+++ app/src/main/java/com/akj/helpyou/activities/ResultRouteActivity.java
@@ -6,11 +6,19 @@ import android.os.Bundle;
 import androidx.annotation.Nullable;
 
 import com.akj.helpyou.R;
+import com.akj.helpyou.activities.Odsay.BusTimetable;
+import com.akj.helpyou.activities.Odsay.FindDirection;
 
 public class ResultRouteActivity extends Activity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_result_road);
+
+        FindDirection fdThread = new FindDirection();
+        fdThread.start();
+
+        BusTimetable btTread = new BusTimetable();
+        btTread.start();
     }
 }
